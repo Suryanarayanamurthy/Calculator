07702eee2fae3fac2986b42fe0713056e8569977
Suryanarayanamurthy <suryanarayan.murthy@gmail.com>
2015-11-17 20:28:55 +0100

    semi working version

--- src/calcApp.js
+++ src/calcApp.js
@@ -1,4 +1,23 @@
 var app = angular.module("CalcApp",['ngRoute']);
-app.controller("CalcController",function(){
+app.controller("CalcController",function($scope){
+$scope.result = "";
+var expression;
+$scope.operand = function(operand){
+    //expression += operand;
+    $scope.result += operand ;
+};
+$scope.operator = function(operator){
+    if(operator === '=')
+    $scope.result = eval($scope.result);
+    else
+    $scope.result += operator ;
 
+    //switch(operator) {
+    //case '=':
+        //result = expression;
+      //  break;
+    //default:
+  //     result;
+//}
+};
 });
